880d96ef1094bbaee270a33ba4c7ab2af6de59d8
tshuskar <nguyenthanhnhon1997ntn@gmail.com>
2025-05-21 14:24:32 +0700

    nop bai tap 3

--- bai_2/bai_tap_quan_ly_ban_hang.sql
+++ bai_2/bai_tap_quan_ly_ban_hang.sql
@@ -4,7 +4,7 @@ use quan_ly_ban_hang;
 create table customer(
 id int primary key auto_increment,
 name varchar(225)  unique,
-age int check (age>18));
+age int check (age>=10));
 
 create table orders(
 id int primary key auto_increment,
@@ -29,3 +29,51 @@ CREATE TABLE order_detail (
    foreign key (product_id) references product(id)
 
 );
+insert into customer (name,age) values 
+('minh quan ',10),
+('ngoc oanh ',20),
+('hong ha ',50);
+
+insert into orders (customer_id,ngay_order) values 
+(1,'2006-3-21'),
+(2,'2006-3-23'),
+(1,'2006-3-26');
+
+ insert into product (product_name ,price) values 
+ ('may giat' ,3),
+ ('tu lanh' ,5),
+ ('dieu hoat' ,7),
+ ('quat' ,1),
+ ('bep dien' ,2);
+ 
+ insert into order_detail(order_id,product_id,order_quality) values
+ (1,1,3),
+ (1,3,7),
+ (1,4,2),
+ (2,1,1),
+ (3,1,8),
+ (2,5,4),
+ (2,3,3);
+ 
+ select o.id,o.ngay_order,o.total_price from orders o;
+ 
+ select c.*,group_concat(p.product_name) as product_name
+ from customer c 
+ join orders o on c.id=o.customer_id 
+ join order_detail od on o.id=od.order_id
+ join product p on p.id=od.product_id
+ group by c.id;
+ 
+ select c.* from customer c left 
+ join orders o on c.id=o.customer_id 
+ where  o.id is null;
+ 
+ select o.id as ma_hoa_don ,o.ngay_order as ngay_ban_hang , sum(od.order_quality*p.price) from orders o 
+ join order_detail od on o.id=od.order_id 
+ join product p on p.id=od.product_id
+ group by o.id,o.ngay_order
+ 
+ 
+
+
+ 
